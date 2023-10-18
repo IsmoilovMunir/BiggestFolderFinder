7ee07e96b0678f9799d14d023686702b4d3f16b9
MunirIsmoilov <ismoilov.munir@gmail.com>
2023-10-18 08:27:01 +0300

    dev

--- src/main/java/FolderSizeCalculator.java
+++ src/main/java/FolderSizeCalculator.java
@@ -1,4 +1,4 @@
-import org.w3c.dom.Node;
+
 
 import java.io.File;
 import java.util.LinkedList;
@@ -6,30 +6,38 @@ import java.util.List;
 import java.util.concurrent.RecursiveTask;
 
 public class FolderSizeCalculator extends RecursiveTask<Long> {
-    private File folder;
+   private Node node;
 
 
-    public FolderSizeCalculator(File folder) {
-        this.folder = folder;
+    public FolderSizeCalculator(Node node) {
+
+        this.node = node;
     }
 
 
     @Override
     protected Long compute() {
+        File folder = node.getFolder();
         if (folder.isFile()) {
-            return folder.length();
+            long length = folder.length();
+            node.setSize(length);
+            return length;
         }
         long sum = 0l;
         List<FolderSizeCalculator> subTasks = new LinkedList<>();
         File[] files = folder.listFiles();
         for (File file : files) {
-            FolderSizeCalculator task = new FolderSizeCalculator(file);
+            Node child = new Node(file);
+            FolderSizeCalculator task = new FolderSizeCalculator(child);
             task.fork();
             subTasks.add(task);
+            node.addChild(child);
+
         }
         for (FolderSizeCalculator task : subTasks) {
             sum += task.join();
         }
+        node.setSize(sum);
         return sum;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,23 +3,22 @@ import java.util.HashMap;
 import java.util.concurrent.ForkJoinPool;
 
 public class Main extends Exception {
-    private static char[] sizeMultipliers = {'B', 'K', 'M', 'G', 'T'};
+
 
     public static void main(String[] args) {
-        System.out.println(getHumanReadableSize(24060));
-        System.exit(0);
-        System.out.println(getSizeFromHumanReadable("1KB"));
-        System.exit(0);
 
-        String folderPath = "/Users/munirismoilov/Desktop/танг";
+        String folderPath = "/Users/munirismoilov/Desktop/25svadb";
         File file = new File(folderPath);
+        Node root = new Node(file);
+
         long start = System.currentTimeMillis();
-        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        FolderSizeCalculator calculator = new FolderSizeCalculator(root);
         ForkJoinPool pool = new ForkJoinPool();
-        long size = pool.invoke(calculator);
-        System.out.println(size);
+       pool.invoke(calculator);
+        System.out.println(root.getSize());
         long duration = System.currentTimeMillis() - start;
         System.out.println(duration + " ms");
+        System.out.println(root.toString());
 //        System.out.println(getFolderSize(file));
 //        Set keys = System.getProperties().keySet();
 //        for (Object key : keys){
@@ -28,42 +27,7 @@ public class Main extends Exception {
 
     }
 
-    //Todo: B, Kb, Mb, Gb,Tb
-    public static String getHumanReadableSize(long size) {
 
-        for (int i = 0; i < sizeMultipliers.length; i++) {
-            double value = size / Math.pow(1024, i);
-            if (value < 1024) {
-                return Math.round(value) +""+ sizeMultipliers[i] + (i > 0 ? "b" : "");
-            }
-        }
-        return "Very Big!";
-    }
-
-    //Todo: B, K, M, G,T
-    // 235K = 240640
-    public static long getSizeFromHumanReadable(String size) {
-        HashMap<Character, Integer> char2multiplier = getMultipliers();
-        char sizeFactor = size
-                .replaceAll("[0-9\\s+]+", "")
-                .charAt(0);
-        int multiplier = char2multiplier.get(sizeFactor);
-        long length = multiplier * Long.valueOf(
-                size.replaceAll("[^0-9]", "")
-        );
-
-        return length;
-    }
-
-    private static HashMap<Character, Integer> getMultipliers() {
-
-        HashMap<Character, Integer> char2multiplier = new HashMap<>();
-        for (int i = 0; i < sizeMultipliers.length; i++) {
-            char2multiplier.put(sizeMultipliers[i], (int) Math.pow(1024, i));
-
-        }
-        return char2multiplier;
-    }
 
     public static long getFolderSize(File folder) {
         if (folder.isFile()) {
